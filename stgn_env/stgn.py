2106a4c34e68d3d9c369fe9cfd2cd37a22e3e9ff
DavidEvangelistaC99 <david.evangelista.c@gmail.com>
2024-08-02 11:45:19 -0500

    Delete stgn_env directory

--- stgn_env/stgn.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-import hiding
-import read
-
-data_send = "The Canny filter is a widely used edge detection algorithm in computer vision, developed by John F. Canny in 1986. It is a multi-stage algorithm that detects a wide range of edges in images, including lines, curves, and corners. The Canny filter is known for its ability to effectively detect edges while reducing noise and false positives."
-
-##---Hiding Data---##
-hiding.hiding_text(data_send,"vegetable_in.png")
